2e513046874ad47af11c5c40b26495c72a1f6ec8
Coco_Cat0 <ars2242@nau.edu>
2026-04-04 02:29:42 -0600

    comments and a training file for model

---
+++ main.py
@@ -0,0 +1,13 @@
+#import Libraries: 
+
+#run main functions
+    #getting the data from public datasets
+    #pre processing data pipeline
+    #load and merge pre processing data
+    #exporatory data analysis
+    # Training K-means clustering model
+
+    # cluster.py
+    # training.py
+    # visualization.py
+#end function
